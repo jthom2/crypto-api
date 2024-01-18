c6767360eae3a486c0e8a993d9ae17e85e80353d
jthom2 <134821369+jthom2@users.noreply.github.com>
2024-01-17 20:14:46 -0600

    Added coin to coin exchange rates

--- main.py
+++ main.py
@@ -30,6 +30,24 @@ async def exchange_rate():
     else:
         return {"message": "Error fetching rate. Please try again later."}
 
+@app.get("/btc_to_eth")
+async def exchange_rate():
+    data = await fetch_btc_to_eth_data()
+    if "rate" in data:
+        rate = data["rate"]
+        return {"message": f"1 BTC = {rate} ETH"}
+    else:
+        return {"message": "Error fetching rate. Please try again later."}
+
+@app.get("/btc_to_ltc")
+async def exchange_rate():
+    data = await fetch_btc_to_ltc_data()
+    if "rate" in data:
+        rate = data["rate"]
+        return {"message": f"1 BTC = {rate} LTC"}
+    else:
+        return {"message": "Error fetching rate. Please try again later."}
+
     
 @app.get("/eth")
 async def exchange_rate():
@@ -40,6 +58,14 @@ async def exchange_rate():
     else:
         return {"message": "Error fetching rate. Please try again later."}    
 
+@app.get("/eth_to_ltc")
+async def exchange_rate():
+    data = await fetch_eth_to_ltc_data()
+    if "rate" in data:
+        rate = data["rate"]
+        return {"message": f"1 ETH = {rate} LTC"}
+    else:
+        return {"message": "Error fetching rate. Please try again later."}
 
 
 async def fetch_btc_data():
@@ -75,7 +101,35 @@ async def fetch_ltc_data():
         response = await client.get(url, headers=headers)
         return response.json()
     
+async def fetch_btc_to_eth_data():
+    url = "https://rest.coinapi.io/v1/exchangerate/BTC/ETH"
+    headers = {
+        "X-CoinAPI-Key": os.environ.get("COIN_API_KEY")
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        return response.json()
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        return response.json()
 
+async def fetch_btc_to_ltc_data():
+    url = "https://rest.coinapi.io/v1/exchangerate/BTC/LTC"
+    headers = {
+        "X-CoinAPI-Key": os.environ.get("COIN_API_KEY")
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        return response.json()
+
+async def fetch_eth_to_ltc_data():
+    url = "https://rest.coinapi.io/v1/exchangerate/ETH/LTC"
+    headers = {
+        "X-CoinAPI-Key": os.environ.get("COIN_API_KEY")
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        return response.json()
 
 
 
